19276c5aec51b46a3af0b235d1bb3b1acf788009
penwa <21547602@qq.com>
2020-10-03 15:56:26 +0800

    first

---
+++ rollup.config.js
@@ -0,0 +1,18 @@
+import json from 'rollup-plugin-json';
+import resolve from 'rollup-plugin-node-resolve';
+import babel from 'rollup-plugin-babel';
+
+export default {
+  input: './src/index.js',
+  output: {
+    file: './dist/bundle.js',
+    format: 'cjs'
+  },
+  plugins: [
+    resolve(),
+    json(),
+    babel({
+      exclude: '/node_modules/**' // 只编译我们的源代码
+    })
+  ]
+};
---
+++ src/index.js
@@ -0,0 +1,5 @@
+'use strict'
+import { version } from '../package.json';
+export default () => {
+  console.log(`version :${version}`);
+}
